d5bf6602103c8a88af552642ca47262ced939a4b
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2023-11-19 08:17:50 -0500

    update

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,10 +1,29 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+import pandas as pd
+import datetime as dt
+%matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
 
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///titanic.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
 
 
 # reflect an existing database into a new model
