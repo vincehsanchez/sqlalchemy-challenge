a2e5503d00b1e9425336d232e839fc9a0e34937f
vincehsanchez <vincehsanchez@vincehsanchezs-MacBook-Air.local>
2023-11-19 20:32:47 -0500

    update

--- SurfsUp/Resources/app_solution.py
+++ SurfsUp/Resources/app_solution.py
@@ -7,12 +7,26 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 #could it not have been working because i had more than one end?
-##from matplotlib import style
+from matplotlib import style ##
 ##style.use('fivethirtyeight')
-##import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt ##
 #################################################
 # Database Setup
 #################################################
+engine = create_engine("sqlite:///hawaii.sqlite") #do i need to desigate folder?
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+# Save references to the table
+Base.classes.keys()
+measurement_ref = Base.classes.measurement
+station_ref = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+#################################################
+# Flask Setup
+##################################################
 # 1. import Flask
 from flask import Flask
 # 2. Create an app, being sure to pass __name__
